0201b2be17d18f09f08a67e1051e2498311ad4d6
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-12-22 09:30:54 +0545

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const switchTheme = () => {
+  let curr_theme = $('#theme').href;
+  if (curr_theme.includes('dark')) {
+    $('#theme').href = curr_theme.split('.').slice(0, 5).join('.') + '.min.css';
+  } else {
+    $('#theme').href = curr_theme.split('.').slice(0, 5).join('.') + '.dark.min.css';
+  }
+}
+
+const mdc = new showdown.Converter({tables: true, tablesHeaderId: true});
+
+const loadGHcontent = () => {
+  fetch("https://cdn.jsdelivr.net/gh/c4p-n1ck/c4p-n1ck/README.md").then( r => { r.text().then( resp => { $('#ghmdc').innerHTML = mdc.makeHtml(resp); } ) } )
+}
+
+$$(() => {
+  // document.getElementById('theme').href = document.getElementById('theme').href.split('.').slice(0,5).join('.') + '.dark.min.css'
+  console.log( "Application loaded successfully.");
+});
