4613c58d3aec006737e06023b546c77b8ad8706b
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-12-22 09:42:10 +0545

    misc changes

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const switchTheme = () => {
   }
 }
 
-const mdc = new showdown.Converter({tables: true, tablesHeaderId: true});
+const mdc = new showdown.Converter({ tables: true, tablesHeaderId: true });
 
 const loadGHcontent = () => {
-  fetch("https://cdn.jsdelivr.net/gh/c4p-n1ck/c4p-n1ck/README.md").then( r => { r.text().then( resp => { $('#ghmdc').innerHTML = mdc.makeHtml(resp); } ) } )
+  fetch("https://cdn.jsdelivr.net/gh/c4p-n1ck/c4p-n1ck@main/README.md").then(r => { r.text().then(resp => { $('#ghmdc').innerHTML = mdc.makeHtml(resp); }) })
 }
 
 $$(() => {
   // document.getElementById('theme').href = document.getElementById('theme').href.split('.').slice(0,5).join('.') + '.dark.min.css'
-  console.log( "Application loaded successfully.");
+  console.log("Application loaded successfully.");
 });
