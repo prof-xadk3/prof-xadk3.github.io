ce70848a85934534b101a7e9ca4e875d2b8f0743
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-01-17 13:50:19 +0545

    semi-conducting-twitter-based-free-commit

--- main.js
+++ main.js
@@ -13,6 +13,30 @@ const loadGHcontent = () => {
   fetch("https://cdn.jsdelivr.net/gh/c4p-n1ck/c4p-n1ck@main/README.md").then(r => { r.text().then(resp => { $('#ghmdc').innerHTML = mdc.makeHtml(resp); }) })
 }
 
+/*
+const switchServiceZ = () => {
+  var cheat = "https://cors-anywhere.herokuapp.com";
+  let url = 'https://rssproxy.migor.org/api/w2f?v=0.1&url=https%3A%2F%2Fground.news%2F&link=.%2Fa%5B1%5D&context=%2F%2Fdiv%5B1%5D%2Fdiv%5B5%5D%2Fdiv%5B2%5D%2Farticle%5B1%5D%2Fdiv%2Fdiv%5B1%5D%2Fdiv%5B3%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv&x=n&re=none&out=atom&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYW5vbnltb3VzIiwiaWF0IjoxNjczOTQwNzI2fQ.61YxMzIcybuXiosKUEhGXND6ecW_-UvQCuLzdcSWn6U';
+  let parser = new RSSParser();
+  parser.parseURL(url, (err, feed) => {
+    if (err) alert(err);
+    console.log(feed.title);
+    feed.items.forEach((entry) => {
+      console.log(entry.title + '~> ' + entry.link);
+    })
+  })
+  let parser = new DOMParser()
+  fetch(url, { mode: 'no-cors' }).then(r => {
+    r.text().then(text => {
+      console.log(text);
+      xmlDoc = parser.parseFromString(text, "text/xml");
+      console.log(xmlDoc);
+      $('#info_wars').innerText = "Loaded?";
+    })
+  });
+}
+*/
+
 aboutX4dk3 = `
 
 `;
