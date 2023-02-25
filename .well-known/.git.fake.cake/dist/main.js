da5c02c78b32417a6e02f96598af27bae5bda0eb
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-02-25 17:41:45 +0545

    .

--- .well-known/.git.fake.cake/dist/main.js
+++ .well-known/.git.fake.cake/dist/main.js
@@ -4,11 +4,11 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const nest_winston_1 = require("nest-winston");
 async function bootstrap() {
-  const app = await core_1.NestFactory.create(app_module_1.AppModule);
-  app.useLogger(app.get(nest_winston_1.WINSTON_MODULE_NEST_PROVIDER));
-  app.setGlobalPrefix(process.env.BASE_PATH || '');
-  await app.listen(process.env.PORT || 2580, process.env.HOST);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useLogger(app.get(nest_winston_1.WINSTON_MODULE_NEST_PROVIDER));
+    app.setGlobalPrefix(process.env.BASE_PATH || '');
+    await app.listen(process.env.PORT || 3000, process.env.HOST);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
