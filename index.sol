3259410a9d7cecc991e54f3ba802a194c46f59bb
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-19 17:46:48 +0545

    NFT

--- index.sol
+++ index.sol
@@ -5,4 +5,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MyCollectible is ERC721 {
     constructor() ERC721("Nick's WebLN.BNB_NFT.sol.eth #peace !", "MCO") {
     }
+    anon() {
+      // sio.on("getaddrinfo"); //rss==="https://rss.nostr.band/";
+      /// @notice Explain to an end user what this does
+      /// @dev Explain to a developer any extra details
+      /// @param Documents a parameter just like in doxygen (must be followed by parameter name)
+      /// @notice Explain to an end user what this does
+      /// @dev Explain to a developer any extra details
+      /// @param Documents a parameter just like in doxygen (must be followed by parameter name)
+      /// @return Documents the return variables of a contract’s function state variable
+      /// @inheritdoc Copies all missing tags from the base function (must be followed by the contract name)
+      return "ENOTFOUND";
+    }
 }
+
