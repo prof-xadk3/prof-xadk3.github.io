03c63d29fbc9c7f3f3f5e357c2d18ea4c749051f
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-19 18:17:52 +0545

    idk /////

--- index.sol
+++ index.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+const sab = new SharedArrayBuffer(1024);
+
 contract MyCollectible is ERC721 {
     constructor() ERC721("Nick's WebLN.BNB_NFT.sol.eth #peace !", "MCO") {
     }
