f7c5afaa2b47608f0752c58f255711346a0b14bc
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-21 02:15:23 +0545

    .

--- connect.js
+++ connect.js
@@ -4,7 +4,7 @@ const magnet = require('magnet-uri');
 const uri = `magnet:?xt=urn:btih:${process.argv[1]}`;
 const parsed = magnet(uri)
 
-console.log(parsed.infoHash) // 'e3811b9539cacff680e418124272177c47477157'
+console.log(parsed.infoHash) // '176419bda44e93a6bd7aeab4ce9d3e27c7a9eb53cbbb60bb7d5c32239140be5c'
 
 const dht = new DHT()
 
