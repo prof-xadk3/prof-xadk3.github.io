773ea0176d18d4b7116d99c3ccf167df5ae34aba
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-25 01:25:53 +0545

    up!

--- frame.it.js
+++ frame.it.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 
-const ARGV = process.argv[1];
+const ARGV = process.argv[2];
 
 console.log(`<iframe border=0 frameborder = 0 height = 250 width = 550 src = "https://twitframe.com/show?url=${ARGV}" ></iframe >`)
