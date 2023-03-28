2c113bcafda53f3d04079558562625457e5e52cd
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-28 07:51:12 +0545

    kudh kara karo yaar :|

--- frame.it.js
+++ frame.it.js
@@ -3,4 +3,4 @@
 
 const ARGV = process.argv[2];
 
-console.log(`<iframe border=0 frameborder = 0 height = 250 width = 550 src = "https://twitframe.com/show?url=${ARGV}" ></iframe >`)
+console.log(`<iframe border=0 frameborder=0 height=250 width=550 src="https://twitframe.com/show?url=${ARGV}" ></iframe >`)
