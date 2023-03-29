286d4a69995dc1e4d0954f189244a26b77a0c232
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-29 13:57:47 +0545

    inking via rusty thars.

---
+++ target/release/deps/jsonrpsee-e2b674eda1d2d6d2.d
@@ -0,0 +1,8 @@
+/home/nick/FAMMstack/portfolio/target/release/deps/jsonrpsee-e2b674eda1d2d6d2.rmeta: /home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/macros.rs
+
+/home/nick/FAMMstack/portfolio/target/release/deps/libjsonrpsee-e2b674eda1d2d6d2.rlib: /home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/macros.rs
+
+/home/nick/FAMMstack/portfolio/target/release/deps/jsonrpsee-e2b674eda1d2d6d2.d: /home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/lib.rs /home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/macros.rs
+
+/home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/lib.rs:
+/home/nick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jsonrpsee-0.16.2/src/macros.rs:
---
+++ target/release/portfolio.d
@@ -0,0 +1,1 @@
+/home/nick/FAMMstack/portfolio/target/release/portfolio: /home/nick/FAMMstack/portfolio/src/main.rs
