1612875970d26dd780477d132599ea9c4067289d
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-30 17:24:05 +0545

    UPDATE>> index.svelte

---
+++ name/0xc9cd0c0e08093cd3e93f89799e65e7bf204b749ea40d89c5bbe25a88191c1183/src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { App, Page, Navbar, Block } from "konsta/svelte";
+</script>
+
+<App theme="ios">
+  <Page>
+    <Navbar title="Nick's App.licatioN" />
+
+    <Block strong>Hello web3!</Block>
+  </Page>
+</App>
