e75eae97d2d144c1cd11cb45349ac23406a6ef47
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-30 17:57:09 +0545

    UPDATE>> whois.PY

---
+++ name/0xc9cd0c0e08093cd3e93f89799e65e7bf204b749ea40d89c5bbe25a88191c1183/whois.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys) == 2:
+  print(sys.argv[0], 'nprofile:ADDR')
+  exit(1)
+
+pub = argv[1]
+if pub.startswith('nprofile:'):
+  pub = pub.split(':')[1]
+
+
+if __name__ == '__main__':
+  __import__('webbrowser').open_new_tab('https://noste.band/u/{pub}');
