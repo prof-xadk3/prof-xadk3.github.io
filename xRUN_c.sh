eb2be1b793eef68791189b5b2e9e4ceb0dc2c693
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-04-03 04:29:44 +0545

    [🔒]-Locked sev3r[m]-local::ver.[BIN]/2580://[🍎] [+] IPV6/64[X]_MD5://[P]?IV=1680475479748

--- xRUN_c.sh
+++ xRUN_c.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+PROTO="https"
+P_PROTO="socks5h"  # up-str-EAM protocol:// .
+LHOST="127.0.0.1";
+NET="safenet"
 TOKEN="d29ybGRsaW5r"
+PARAM="host"
 TLD="com"
 CC="np"
 PROVIDER="$(echo $TOKEN | base64 -d)"
 HEADER_P="$1"
 PORT="1337"
+TS="$(deno eval 'console.log(+new Date)' | cat)";  # Time\/(act./TIVITY)-Tracker!
 
 if [[ -n $HEADER_P ]]; then
   echo "Please enter \$PATH to the JSonP?iv=?"
   exit 0;
 fi
 
-curl -vx "socks5h://127.0.0.1:$PORT" "https://safenet.$PROVIDER.$TLD.$CC?host=$HEADER_P"
+curl -vx "$P_PROTO://$LHOST:$PORT" "$PROTO://$NET.$PROVIDER.$TLD.$CC?$PARAM=$HEADER_P?IV=$TS"
