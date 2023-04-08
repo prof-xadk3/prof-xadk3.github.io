bf66d15bb131c9ba19dd9218442564cde78db822
root <root@localhost.localdomain>
2023-04-08 13:13:15 +0000

    xURL.RUN.DEMNMO!

--- xRUN_c.sh
+++ xRUN_c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROTO="https"; P_PROTO="socks5h"  # up-str-EAM protocol:// .
-LHOST="127.0.0.1"; NET="safenet"
+LHOST="127.0.0.1"; NET="safenet"; FKIT="blockdomain"
 TOKEN="d29ybGRsaW5r"; PARAM="host"; TLD="com"; CC="np"
 PROVIDER="$(echo $TOKEN | base64 -d)"
 HEADER_P="$1"; PORT="1337"
@@ -11,3 +11,4 @@ if [[ -n $HEADER_P ]]; then
   echo "[-] Please enter \$PATH to the JSonP?iv=?"
   exit 1;
 fi; curl -vx "$P_PROTO://$LHOST:$PORT" "$PROTO://$NET.$PROVIDER.$TLD.$CC?$PARAM=$HEADER_P?IV=$TS" -o - | less
+curl -sx "$P_PROTO://$LHOST:$PORT" "$PROTO://$FKIT.$PROVIDER.$TLD.$CC?$PARAM=$HEADER_P?IV=$TS"
